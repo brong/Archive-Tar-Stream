d85c9d8ca0bbe2913b14109c3fd7aa0aafab5f52
Bron Gondwana <brong@opera.com>
2011-10-06 20:00:41 +0200

    module-starter boilerplate

---
+++ lib/Archive/Tar/Stream.pm
@@ -0,0 +1,111 @@
+package Archive::Tar::Stream;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+Archive::Tar::Stream - The great new Archive::Tar::Stream!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Archive::Tar::Stream;
+
+    my $foo = Archive::Tar::Stream->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Bron Gondwana, C<< <perlcode at brong.net> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-archive-tar-stream at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Archive-Tar-Stream>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Archive::Tar::Stream
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Archive-Tar-Stream>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Archive-Tar-Stream>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Archive-Tar-Stream>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Archive-Tar-Stream/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2011 Bron Gondwana.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of Archive::Tar::Stream
