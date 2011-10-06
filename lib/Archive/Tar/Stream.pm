39b97386e03d495ceb6f80b6e6ddfba401accbba
Bron Gondwana <brong@opera.com>
2011-10-06 21:32:45 +0200

    Add Documentation

--- lib/Archive/Tar/Stream.pm
+++ lib/Archive/Tar/Stream.pm
@@ -4,6 +4,9 @@ use 5.006;
 use strict;
 use warnings;
 
+# this is pretty fixed by the format!
+use constant BLOCKSIZE => 512;
+
 # dependencies
 use IO::File;
 use IO::Handle;
@@ -37,6 +40,10 @@ one purely for writes.  It does no seeking, it just unpacks
 individual records from the input filehandle, and packs records
 to the output filehandle.
 
+This module does not attempt to do any file handle management or
+compression for you.  External zcat and gzip are quite fast and
+use separate cores.
+
     use Archive::Tar::Stream;
 
     my $ts = Archive::Tar::Stream->new(outfh => $fh);
@@ -68,15 +75,37 @@ to the output filehandle.
 
 =head2 new
 
-docs soon
+    my $ts = Archive::Tar::Stream->new(%args);
+
+Args:
+   infh      - filehandle to read from
+   outfh     - filehandle to write to
+   inpos     - initial offset in infh
+   outpos    - initial offset in outfh
+   safe_copy - boolean.
+
+Offsets are for informational purposes only, but can be
+useful if you are tracking offsets of items within your
+tar files separately.  All read and write functions
+update these offsets.  If you don't provide offsets, they
+will default to zero.
+
+Safe Copy is the default - you have to explicitly turn it
+off.  If Safe Copy is set, every file is first extracted
+from the input filehandle and stored in a temporary file
+before appending to the output filehandle.  This uses
+slightly more IO, but guarantees that a truncated input
+file will not corrupt the output file.
 
 =cut
-
-use constant BLOCKSIZE => 512;
 
 sub new {
   my $class = shift;
   my %args = @_;
+
+  die "Useless to stream without a filehandle"
+    unless ($args{infh} or $args{outfh});
+
   my $Self = bless {
     # defaults
     safe_copy => 1,
@@ -84,8 +113,17 @@ sub new {
     outpos => 0,
     %args
   }, ref($class) || $class;
+
   return $Self;
 }
+
+=head2 SafeCopy
+
+   $ts->SafeCopy(0);
+
+Toggle the "safe_copy" field mentioned above.
+
+=cut
 
 sub SafeCopy {
   my $Self = shift;
@@ -94,6 +132,15 @@ sub SafeCopy {
   }
   return $Self->{safe_copy};
 }
+
+=head2 InPos
+
+=head2 OutPos
+
+Read only accessors for the internal position trackers for
+the two tar streams.
+
+=cut
 
 sub InPos {
   my $Self = shift;
@@ -105,233 +152,111 @@ sub OutPos {
   return $Self->{outpos};
 }
 
-sub ReadBlocks {
+=head2 AddFile
+
+Adds a file to the output filehandle, adding sensible
+defaults for all the extra header fields.
+
+Requires: outfh
+
+   my $header = $ts->AddFile($name, $size, $fh, %extra);
+
+See TARHEADER for documentation of the header fields.
+
+You must provide 'size' due to the non-seeking nature of
+this library, but "-s $fh" is usually fine.
+
+Returns the complete header that was written.
+
+=cut
+
+sub AddFile {
   my $Self = shift;
-  my $nblocks = shift || 1;
-  unless ($Self->{infh}) {
-    die "Attempt to read without input filehandle";
-  }
-  my $bytes = BLOCKSIZE * $nblocks;
-  my $buf;
-  my @result;
-  while ($bytes > 0) {
-    my $n = sysread($Self->{infh}, $buf, $bytes);
-    unless ($n) {
-      delete $Self->{infh};
-      return if ($bytes == BLOCKSIZE * $nblocks); # nothing at EOF
-      die "Failed to read full block at $Self->{inpos}";
-    }
-    $bytes -= $n;
-    $Self->{inpos} += $n;
-    push @result, $buf;
-  }
-  return join('', @result);
+  my $name = shift;
+  my $size = shift;
+  my $fh = shift;
+
+  my $header = $Self->BlankHeader(@_);
+  $header->{name} = $name;
+  $header->{size} = $size;
+
+  my $fullheader = $Self->WriteHeader($header);
+  $Self->CopyFromFh($fh, $size);
+
+  return $fullheader;
 }
 
-sub WriteBlocks {
+=head2 AddLink
+
+   my $header = $ts->AddLink($name, $linkname, %extra);
+
+Adds a symlink to the output filehandle.
+
+See TARHEADER for documentation of the header fields.
+
+Returns the complete header that was written.
+
+=cut
+
+sub AddLink {
   my $Self = shift;
-  my $string = shift;
-  my $nblocks = shift || 1;
+  my $name = shift;
+  my $linkname = shift;
 
-  my $bytes = BLOCKSIZE * $nblocks;
+  my $header = $Self->BlankHeader(@_);
+  $header->{name} = $name;
+  $header->{linkname} = $linkname;
 
-  unless ($Self->{outfh}) {
-    die "Attempt to write without output filehandle";
-  }
-  my $pos = $Self->{outpos};
-
-  # make sure we've got $nblocks times BLOCKSIZE bytes to write
-  if (length($string) > $bytes) {
-    $string = substr($string, 0, $bytes);
-  }
-  elsif (length($string) < $bytes) {
-    $string .= "\0" x ($bytes - length($string));
-  }
-
-  while ($bytes > 0) {
-    my $n = syswrite($Self->{outfh}, $string, $bytes, (BLOCKSIZE * $nblocks) - $bytes);
-    unless ($n) {
-      delete $Self->{outfh};
-      die "Failed to write full block at $Self->{outpos}";
-    }
-    $bytes -= $n;
-    $Self->{outpos} += $n;
-  }
-
-  return $pos;
+  return $Self->WriteHeader($header);
 }
 
-sub ReadHeader {
-  my $Self = shift;
-  my %Opts = @_;
+=head2 StreamCopy
 
-  my ($pos, $header, $skipped) = (0, undef, 0);
+Streams all records from the input filehandle and provides
+an easy way to write them to the output filehandle.
 
-  my $initialpos = $Self->{inpos};
-  while (not $header) {
-    $pos = $Self->{inpos};
-    my $block = $Self->ReadBlocks();
-    last unless $block;
-    $header = $Self->ParseHeader($block);
-    last if $header;
-    last unless $Opts{SkipInvalid};
-    $skipped++;
-  }
+Requires: infh
+Optional: outfh - required if you return 'KEEP'
 
-  return unless $header;
+    $ts->StreamCopy(sub {
+        my ($header, $outpos, $fh) = @_;
+        # ...
+        return 'KEEP';
+    });
 
-  if ($skipped) {
-    warn "Skipped $skipped blocks - invalid headers at $initialpos\n";
-  }
+The chooser function can either return a single 'action' or
+a tuple of action and a new header.
 
-  $header->{_pos} = $pos;
-  $Self->{last_header} = $header;
+The action can be:
+   KEEP - copy this file as is (possibly changed header) to output tar
+   EDIT - re-call $Chooser with filehandle
+   SKIP - skip over the file and call $Chooser on the next one
 
-  return $header;
-}
+EDIT mode:
 
-sub WriteHeader {
-  my $Self = shift;
-  my $header = shift;
+the file will be copied to a temporary file and the filehandle passed to
+$Chooser.  It can truncate, rewrite, edit - whatever.  So long as it updates
+$header->{size} and returns it as $newheader it's all good.
 
-  my $block = $Self->CreateHeader($header);
-  my $pos = $Self->WriteBlocks($block);
-  return( {%$header, _pos => $pos} );
-}
+you don't have to change the file of course, it's also good just as a way to
+view the contents of some files as you stream them.
 
-sub ParseHeader {
-  my $Self = shift;
-  my $block = shift;
+A standard usage pattern looks like this:
 
-  # enforce length
-  return unless(512 == length($block));
+  $ts->StreamCopy(sub {
+    my ($header, $outpos, $fs) = @_;
 
-  # skip empty blocks
-  return if substr($block, 0, 1) eq "\0";
+    # simple checks
+    return 'KEEP' if do_want($header);
+    return 'SKIP' if dont_want($header);
 
-  # unpack exactly 15 items from the block
-  my @items = unpack("a100a8a8a8a12a12a8a1a100a8a32a32a8a8a155", $block);
-  return unless (15 == @items);
+    return 'EDIT' unless $fh;
 
-  for (@items) {
-    s/\0.*//; # strip from first null
-  }
+    # checks that require a filehandle
+  });
 
-  my $chksum = oct($items[6]);
-  # do checksum
-  substr($block, 148, 8) = "        ";
-  unless (unpack("%16C*", $block) == $chksum) {
-    return;
-  }
+=cut
 
-  my %header = (
-    name => $items[0],
-    mode => oct($items[1]),
-    uid => oct($items[2]),
-    gid => oct($items[3]),
-    size => oct($items[4]),
-    mtime => oct($items[5]),
-    # checksum
-    typeflag => $items[7],
-    linkname => $items[8],
-    # magic
-    uname => $items[10],
-    gname => $items[11],
-    devmajor => oct($items[12]),
-    devminor => oct($items[13]),
-    prefix => $items[14],
-  );
-
-  return \%header;
-}
-
-sub BlankHeader {
-  my $Self = shift;
-  my %hash = (
-    name => '',
-    mode => 0777,
-    uid => 0,
-    gid => 0,
-    size => 0,
-    mtime => time(),
-    typeflag => '0', # this is actually the STANDARD plain file format, phooey.  Not 'f' like Tar writes
-    linkname => '',
-    uname => '',
-    gname => '',
-    devmajor => 0,
-    devminor => 0,
-    prefix => '',
-  );
-  my %overrides = @_;
-  foreach my $key (keys %overrides) {
-    if (exists $hash{$key}) {
-      $hash{$key} = $overrides{$key};
-    }
-    else {
-      warn "invalid key $key for tar header\n";
-    }
-  }
-  return \%hash;
-}
-
-sub CreateHeader {
-  my $Self = shift;
-  my $header = shift;
-
-  my $block = pack("a100a8a8a8a12a12a8a1a100a8a32a32a8a8a155",
-    $header->{name},
-    sprintf("%07o", $header->{mode}),
-    sprintf("%07o", $header->{uid}),
-    sprintf("%07o", $header->{gid}),
-    sprintf("%011o", $header->{size}),
-    sprintf("%011o", $header->{mtime}),
-    "        ",                  # chksum
-    $header->{typeflag},
-    $header->{linkname},
-    "ustar  \0",                   # magic
-    $header->{uname},
-    $header->{gname},
-    sprintf("%07o", $header->{devmajor}),
-    sprintf("%07o", $header->{devminor}),
-    $header->{prefix},
-  );
-
-  # calculate checksum
-  my $checksum = sprintf("%06o", unpack("%16C*", $block));
-  substr($block, 148, 8) = $checksum . "\0 ";
-
-  # pad out to BLOCKSIZE characters
-  if (length($block) < BLOCKSIZE) {
-    $block .= "\0" x (BLOCKSIZE - length($block));
-  }
-  elsif (length($block) > BLOCKSIZE) {
-    $block = substr($block, 0, BLOCKSIZE);
-  }
-
-  return $block;
-}
-
-# Chooser interface:
-#
-# $Chooser->($header, $outpos, undef);
-# returns two values:
-# ($status, $newheader)
-#
-# if $newheader is specified, it will be used instead.
-#
-# $status can be:
-#   EXIT - quit streaming at this point, ignore this item
-#   KEEP - copy this file as is (possibly changed header) to output tar
-#   EDIT - re-call $Chooser with filehandle
-#   SKIP - skip over the file and call $Chooser on the next one
-#
-# EDIT mode:
-# the file will be copied to a temporary file and the filehandle passed to
-# $Chooser.  It can truncate, rewrite, edit - whatever.  So long as it updates
-# $header->{size} and returns it as $newheader it's all good.
-#
-# you don't have to change the file of course, it's also good just as a way to
-# view the contents of some files as you stream them
 sub StreamCopy {
   my $Self = shift;
   my $Chooser = shift;
@@ -345,7 +270,7 @@ sub StreamCopy {
       my $Edited;
 
       # positive code means read the file
-      if ($rc eq 'EDIT') { 
+      if ($rc eq 'EDIT') {
         $Edited = 1;
         $TempFile = $Self->CopyToTempFile($header->{size});
         # call chooser again with the contents
@@ -405,33 +330,316 @@ sub StreamCopy {
   }
 }
 
-sub AddFile {
+=head2 ReadBlocks
+
+Requires: infh
+
+   my $raw = $ts->ReadBlocks($nblocks);
+
+Reads 'n' blocks of 512 bytes from the input filehandle
+and returns them as single scalar.
+
+Returns undef at EOF on the input filehandle.  Any further
+calls after undef is returned will die.  This is to avoid
+naive programmers creating infinite loops.
+
+nblocks is optional, and defaults to 1.
+
+=cut
+
+sub ReadBlocks {
   my $Self = shift;
-  my $name = shift;
-  my $size = shift;
-  my $fh = shift;
-
-  my $header = $Self->BlankHeader(@_);
-  $header->{name} = $name;
-  $header->{size} = $size;
-
-  my $fullheader = $Self->WriteHeader($header);
-  $Self->CopyFromFh($fh, $size);
-
-  return $fullheader;
+  my $nblocks = shift || 1;
+  unless ($Self->{infh}) {
+    die "Attempt to read without input filehandle";
+  }
+  my $bytes = BLOCKSIZE * $nblocks;
+  my $buf;
+  my @result;
+  while ($bytes > 0) {
+    my $n = sysread($Self->{infh}, $buf, $bytes);
+    unless ($n) {
+      delete $Self->{infh};
+      return if ($bytes == BLOCKSIZE * $nblocks); # nothing at EOF
+      die "Failed to read full block at $Self->{inpos}";
+    }
+    $bytes -= $n;
+    $Self->{inpos} += $n;
+    push @result, $buf;
+  }
+  return join('', @result);
 }
 
-sub AddLink {
+=head2 WriteBlocks
+
+Requires: outfh
+
+   my $pos = $ts->WriteBlocks($buffer, $nblocks);
+
+Write blocks to the output filehandle.  If the buffer is too
+short, it will be padded with zero bytes.  If it's too long,
+it will be truncated.
+
+nblocks is optional, and defaults to 1.
+
+Returns the position of the header in the output stream.
+
+=cut
+
+sub WriteBlocks {
   my $Self = shift;
-  my $name = shift;
-  my $linkname = shift;
+  my $string = shift;
+  my $nblocks = shift || 1;
 
-  my $header = $Self->BlankHeader(@_);
-  $header->{name} = $name;
-  $header->{linkname} = $linkname;
+  my $bytes = BLOCKSIZE * $nblocks;
 
-  return $Self->WriteHeader($header);
+  unless ($Self->{outfh}) {
+    die "Attempt to write without output filehandle";
+  }
+  my $pos = $Self->{outpos};
+
+  # make sure we've got $nblocks times BLOCKSIZE bytes to write
+  if (length($string) > $bytes) {
+    $string = substr($string, 0, $bytes);
+  }
+  elsif (length($string) < $bytes) {
+    $string .= "\0" x ($bytes - length($string));
+  }
+
+  while ($bytes > 0) {
+    my $n = syswrite($Self->{outfh}, $string, $bytes, (BLOCKSIZE * $nblocks) - $bytes);
+    unless ($n) {
+      delete $Self->{outfh};
+      die "Failed to write full block at $Self->{outpos}";
+    }
+    $bytes -= $n;
+    $Self->{outpos} += $n;
+  }
+
+  return $pos;
 }
+
+=head2 ReadHeader
+
+Requires: infh
+
+   my $header = $ts->ReadHeader(%Opts);
+
+Read a single 512 byte header off the input filehandle and
+convert it to a TARHEADER format hashref.  Returns undef
+at the end of the file.
+
+If the option (SkipInvalid => 1) is passed, it will skip
+over blocks which fail to pass the checksum test.
+
+=cut
+
+sub ReadHeader {
+  my $Self = shift;
+  my %Opts = @_;
+
+  my ($pos, $header, $skipped) = (0, undef, 0);
+
+  my $initialpos = $Self->{inpos};
+  while (not $header) {
+    $pos = $Self->{inpos};
+    my $block = $Self->ReadBlocks();
+    last unless $block;
+    $header = $Self->ParseHeader($block);
+    last if $header;
+    last unless $Opts{SkipInvalid};
+    $skipped++;
+  }
+
+  return unless $header;
+
+  if ($skipped) {
+    warn "Skipped $skipped blocks - invalid headers at $initialpos\n";
+  }
+
+  $header->{_pos} = $pos;
+  $Self->{last_header} = $header;
+
+  return $header;
+}
+
+=head2 WriteHeader
+
+Requires: outfh
+
+   my $newheader = $ts->WriteHeader($header);
+
+Read a single 512 byte header off the input filehandle.
+
+If the option (SkipInvalid => 1) is passed, it will skip
+over blocks which fail to pass the checksum test.
+
+Returns a copy of the header with _pos set to the position
+in the output file.
+
+=cut
+
+sub WriteHeader {
+  my $Self = shift;
+  my $header = shift;
+
+  my $block = $Self->CreateHeader($header);
+  my $pos = $Self->WriteBlocks($block);
+  return( {%$header, _pos => $pos} );
+}
+
+=head2 ParseHeader
+
+   my $header = $ts->ParseHeader($block);
+
+Parse a single block of raw bytes into a TARHEADER
+format header.  $block must be exactly 512 bytes.
+
+Returns undef if the block fails the checksum test.
+
+=cut
+
+sub ParseHeader {
+  my $Self = shift;
+  my $block = shift;
+
+  # enforce length
+  return unless(512 == length($block));
+
+  # skip empty blocks
+  return if substr($block, 0, 1) eq "\0";
+
+  # unpack exactly 15 items from the block
+  my @items = unpack("a100a8a8a8a12a12a8a1a100a8a32a32a8a8a155", $block);
+  return unless (15 == @items);
+
+  for (@items) {
+    s/\0.*//; # strip from first null
+  }
+
+  my $chksum = oct($items[6]);
+  # do checksum
+  substr($block, 148, 8) = "        ";
+  unless (unpack("%16C*", $block) == $chksum) {
+    return;
+  }
+
+  my %header = (
+    name => $items[0],
+    mode => oct($items[1]),
+    uid => oct($items[2]),
+    gid => oct($items[3]),
+    size => oct($items[4]),
+    mtime => oct($items[5]),
+    # checksum
+    typeflag => $items[7],
+    linkname => $items[8],
+    # magic
+    uname => $items[10],
+    gname => $items[11],
+    devmajor => oct($items[12]),
+    devminor => oct($items[13]),
+    prefix => $items[14],
+  );
+
+  return \%header;
+}
+
+=head2 BlankHeader
+
+  my $header = $ts->BlankHeader(%extra);
+
+Create a header with sensible defaults.  That means
+time() for mtime, 0777 for mode, etc.
+
+It then applies any 'extra' fields from %extra to
+generate a final header.  Also validates the keys
+in %extra to make sure they're all known keys.
+
+=cut
+
+sub BlankHeader {
+  my $Self = shift;
+  my %hash = (
+    name => '',
+    mode => 0777,
+    uid => 0,
+    gid => 0,
+    size => 0,
+    mtime => time(),
+    typeflag => '0', # this is actually the STANDARD plain file format, phooey.  Not 'f' like Tar writes
+    linkname => '',
+    uname => '',
+    gname => '',
+    devmajor => 0,
+    devminor => 0,
+    prefix => '',
+  );
+  my %overrides = @_;
+  foreach my $key (keys %overrides) {
+    if (exists $hash{$key}) {
+      $hash{$key} = $overrides{$key};
+    }
+    else {
+      warn "invalid key $key for tar header\n";
+    }
+  }
+  return \%hash;
+}
+
+=head2 CreateHeader
+
+   my $block = $ts->CreateHeader($header);
+
+Creates a 512 byte block from the TARHEADER format header.
+
+=cut
+
+sub CreateHeader {
+  my $Self = shift;
+  my $header = shift;
+
+  my $block = pack("a100a8a8a8a12a12a8a1a100a8a32a32a8a8a155",
+    $header->{name},
+    sprintf("%07o", $header->{mode}),
+    sprintf("%07o", $header->{uid}),
+    sprintf("%07o", $header->{gid}),
+    sprintf("%011o", $header->{size}),
+    sprintf("%011o", $header->{mtime}),
+    "        ",                  # chksum
+    $header->{typeflag},
+    $header->{linkname},
+    "ustar  \0",                   # magic
+    $header->{uname},
+    $header->{gname},
+    sprintf("%07o", $header->{devmajor}),
+    sprintf("%07o", $header->{devminor}),
+    $header->{prefix},
+  );
+
+  # calculate checksum
+  my $checksum = sprintf("%06o", unpack("%16C*", $block));
+  substr($block, 148, 8) = $checksum . "\0 ";
+
+  # pad out to BLOCKSIZE characters
+  if (length($block) < BLOCKSIZE) {
+    $block .= "\0" x (BLOCKSIZE - length($block));
+  }
+  elsif (length($block) > BLOCKSIZE) {
+    $block = substr($block, 0, BLOCKSIZE);
+  }
+
+  return $block;
+}
+
+=head2 CopyBytes
+
+   $ts->CopyBytes($bytes);
+
+Copies bytes from input to output filehandle, rounded up to
+block size, so only whole blocks are actually copied.
+
+=cut
 
 sub CopyBytes {
   my $Self = shift;
@@ -446,6 +654,16 @@ sub CopyBytes {
   }
 }
 
+=head2 DumpBytes
+
+   $ts->DumpBytes($bytes);
+
+Just like CopyBytes, but it doesn't write anywhere.
+Reads full blocks off the input filehandle, rounding
+up to block size.
+
+=cut
+
 sub DumpBytes {
   my $Self = shift;
   my $bytes = shift;
@@ -457,6 +675,18 @@ sub DumpBytes {
   }
 }
 
+=head2 FinishTar
+
+   $ts->FinishTar();
+
+Writes 5 blocks of zero bytes to the output file, which makes
+gnu tar happy that it's found the end of the file.
+
+Don't use this if you're planning on concatenating multiple
+files together.
+
+=cut
+
 sub FinishTar {
   my $Self = shift;
   $Self->WriteBlocks("\0" x 512);
@@ -465,6 +695,16 @@ sub FinishTar {
   $Self->WriteBlocks("\0" x 512);
   $Self->WriteBlocks("\0" x 512);
 }
+
+=head2 CopyToTempFile
+
+   my $fh = $ts->CopyToTempFile($header->{size});
+
+Creates a temporary file (with File::Temp) and fills it with
+the contents of the file on the input stream.  It reads
+entire blocks, and discards the padding.
+
+=cut
 
 sub CopyToTempFile {
   my $Self = shift;
@@ -483,6 +723,15 @@ sub CopyToTempFile {
 
   return $TempFile;
 }
+
+=head2 CopyFromFh
+
+   $ts->CopyFromFh($fh, $header->{size});
+
+Copies the contents of the filehandle to the output stream,
+padding out to block size.
+
+=cut
 
 sub CopyFromFh {
   my $Self = shift;
@@ -509,9 +758,6 @@ sub CopyFromFh {
   }
 }
 
-
-# Finally,
-1;
 
 =head1 AUTHOR
 
