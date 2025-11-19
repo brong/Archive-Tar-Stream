27d61551ad3760d87584b67ee51a04ad35301434
Bron Gondwana <brong@fastmailteam.com>
2025-11-19 13:31:00 -0500

    Release 0.4
    Also update changes for 0.3

--- lib/Archive/Tar/Stream.pm
+++ lib/Archive/Tar/Stream.pm
@@ -24,11 +24,11 @@ Archive::Tar::Stream - pure perl IO-friendly tar file management
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 
 =head1 SYNOPSIS
